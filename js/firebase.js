902a642567a8bd91d4e8f8e995834b821bfb066f
Marcelo-arg-web <congrevillafiad@gmail.com>
2026-02-27 00:20:29 -0300

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
 
-export const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "discursos-147ba.firebaseapp.com",
+  projectId: "discursos-147ba",
+  storageBucket: "discursos-147ba.appspot.com",
+  messagingSenderId: "778635238055",
+  appId: "1:778635238055:web:100e08b496f0b6d6c35982"
+};
+
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
